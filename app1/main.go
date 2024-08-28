69691b181c9922205cce7e5c96e43107d5e929e0
ekokurniawann <ekokurniawaann@gmail.com>
2024-08-29 00:32:45 +0700

    add flag for content and port in go server

--- app1/main.go
+++ app1/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	content := flag.String("content", "Hello, World!", "Content to display")
+	port := flag.String("port", "8080", "Port to run the server on")
+	flag.Parse()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello Golang")
+		fmt.Fprintf(w, *content)
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Printf("Starting server on port %s...", *port)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
